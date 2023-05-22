3ececa2a331f49258ac2d82f7c1711a1de5141c2
宋子健 <ken.song@hitpointcloud.com>
2023-05-22 17:32:13 +0800

    chore：添加form data

--- fastapi_tutorial/router/product.py
+++ fastapi_tutorial/router/product.py
@@ -1,5 +1,5 @@
 from typing import Optional, List
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, Cookie, Form
 from fastapi.responses import Response, PlainTextResponse,HTMLResponse
 router = APIRouter(
     prefix='/product',
@@ -7,17 +7,30 @@ router = APIRouter(
 )
 
 products = ['watch', 'camera','phone']
+@router.post('/new')
+def create_product(name: str = Form(...)):
+    products.append(name)
+    return products
 @router.get('/all')
 def get_all_products():
     # return products
     data = " ".join(products)
-    return Response(content=data, media_type="text/plain")
+    response = Response(content=data, media_type="text/plain")
+    response.set_cookie(key="test_cookie", value ="test_cookie_value")
+    return response
 @router.get('/withheader')
 def get_product(    
     response: Response,
-    custom_header: Optional[List[str]] = Header(None) 
+    custom_header: Optional[List[str]] = Header(None),
+    test_cookie: Optional[str] = Cookie(None)
     ):
-    return products
+    if custom_header:
+        response.headers['custom_response_header'] = " and ".join(custom_header)
+    return {
+        'data':products,
+        'custom_header': custom_header,
+        'my_cookie':test_cookie
+    }
 @router.get("/{id}", responses={
     200:{
         "content":{
