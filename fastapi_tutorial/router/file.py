50b2c6cc0dfb6c862ac270a47cbd0eae8a10b269
宋子健 <ken.song@hitpointcloud.com>
2023-06-12 17:24:57 +0800

    feat: 添加文件内容

---
+++ fastapi_tutorial/router/file.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+from fastapi import APIRouter, File, UploadFile
+from fastapi.responses import FileResponse
+router = APIRouter(
+    prefix='/file',
+    tags=['file']
+)
+
+@router.post('/file')
+def get_file(file:bytes = File(...)):
+    content =file.decode('utf-8')
+    lines = content.split('\n')
+    return {'lines':lines}
+
+@router.post('/uploadfile')
+def get_uploadfile(upload_file: UploadFile = File(...)):
+    path = os.path.join("files", upload_file.filename)
+    with open(path,'w+b') as buffer:
+        shutil.copyfileobj(upload_file.file, buffer)
+    return {
+        'filename': upload_file.filename,
+        'type': upload_file.content_type
+    }
+
+@router.get('/download/{name}', response_class=FileResponse)
+def get_file(name:str):
+    path = os.path.join("files", name)
+    return path
+    
