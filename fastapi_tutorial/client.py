ecabb4d57b1641a458be75d64da941594a7c6658
宋子健 <ken.song@hitpointcloud.com>
2023-06-14 18:13:51 +0800

    chore: 端口改回8000

--- fastapi_tutorial/client.py
+++ fastapi_tutorial/client.py
@@ -13,7 +13,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8080/chat");
+            var ws = new WebSocket("ws://localhost:8000/chat");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
